eebcfbef3ecc8107c9e765293123ebf885dbb9cf
stefania m <shin3qi@gmail.com>
2024-07-31 23:36:46 +0300

    beggining done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,8 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
+        background: '#37B7C3',
       }}
     >
       React homework template
